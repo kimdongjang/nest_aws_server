1ee3cb4aaf101e5a80bbfd352a35ec8fd4c2c155
jwk <jwk@power21.co.kr>
2022-03-24 16:57:36 +0900

    next js 추가

---
+++ apps/next-client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>Welcome to Next.js!</div>
+  )
+}
+
+export default Home
