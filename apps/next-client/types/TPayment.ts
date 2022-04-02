b5490f6d117fbf16e28955b3069b8ce4cd502d0e
kimdongjang <naru_oo@naver.com>
2022-04-02 19:56:39 +0900

    아임포트 결제 기능 준비중

---
+++ apps/next-client/types/TPayment.ts
@@ -0,0 +1,12 @@
+export type TPayment = {
+    pg: string,
+    pay_method: string,
+    merchant_uid: string,
+    amount: number,
+    name: string,
+    buyer_name: string,
+    buyer_tel: string,
+    buyer_email: string,
+    buyer_addr: string,
+    buyer_postcode: string,
+}
