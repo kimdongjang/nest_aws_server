035f3b680da50fa254362b0668a063a898216af3
jwk <jwk@power21.co.kr>
2022-05-23 17:42:10 +0900

    .

--- apps/mq-server-nest/src/main.ts
+++ apps/mq-server-nest/src/main.ts
@@ -16,18 +16,13 @@ async function bootstrap() {
 
   // app.listen();
 
-
   const port = process.env.PORT ? Number(process.env.PORT) : 5672;
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.TCP,
-    options: {
-      host: '0.0.0.0',
-      port,
-    },
+    options: { retryAttempts: 5, retryDelay: 3000, port: port },
   });
   await app.listen();
-
-
+  console.log(`Application is running on: ${port}`);
 
   // const app = await NestFactory.create(AppModule);
   // app.connectMicroservice<MicroserviceOptions>({
@@ -38,6 +33,5 @@ async function bootstrap() {
   // await app.startAllMicroservices();
   // await app.listen(3001);
   // console.log(`Application is running on: ${await app.getUrl()}`);
-
 }
 bootstrap();
