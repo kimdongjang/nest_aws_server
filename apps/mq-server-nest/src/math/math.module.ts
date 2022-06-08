4d3224238993a37ef17464e842a076aa6ad44144
jwk <jwk@power21.co.kr>
2022-06-08 18:00:41 +0900

    .

--- apps/mq-server-nest/src/math/math.module.ts
+++ apps/mq-server-nest/src/math/math.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ClientProxyFactory, ClientsModule, Transport } from '@nestjs/microservices';
+import {
+  ClientProxyFactory,
+  ClientsModule,
+  Transport,
+} from '@nestjs/microservices';
 import { MATH_SERVICE } from './math.constants';
 import { MathController } from './math.controller';
 
 @Module({
-    imports: [
-        // name 속성은 필요한 곳에 인스턴스를 주입하는데 사용할 수 있는 토큰 역할
-        ClientsModule.register([{ name: MATH_SERVICE, transport: Transport.TCP }]),
-    ],
-    controllers: [MathController],
+  imports: [
+    // name 속성은 필요한 곳에 인스턴스를 주입하는데 사용할 수 있는 토큰 역할
+    ClientsModule.register([{ name: MATH_SERVICE, transport: Transport.TCP }]),
+  ],
+  controllers: [MathController],
 })
-export class MathModule { }
+export class MathModule {}
