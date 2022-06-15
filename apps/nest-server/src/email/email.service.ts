97913cf08c3bbc77dacb8466dccacd05108d3519
jwk <jwk@power21.co.kr>
2022-06-15 09:35:55 +0900

    fixed untracked files

--- apps/nest-server/src/email/email.service.ts
+++ apps/nest-server/src/email/email.service.ts
@@ -7,14 +7,9 @@ import { Repository } from "typeorm";
 @Injectable()
 export class EmailService {
   private transporter: Mail; // 이메일을 전송하기 위한 노드 메일러 사용
-  constructor(private readonly mailerService: MailerService) { }
+  constructor(private readonly mailerService: MailerService) {}
 
-  async _send(
-    tos: string[],
-    subject: string,
-    templateName: string,
-    context: any = {}
-  ): Promise<boolean> {
+  async _send(tos: string[], subject: string, templateName: string, context: any = {}): Promise<boolean> {
     await this.mailerService.sendMail({
       to: tos.join(", "),
       subject,
@@ -26,10 +21,7 @@ export class EmailService {
   }
 
   // 가입 요청 확인용 이메일 전송
-  async sendMemberJoinVerification(
-    emailAddress: string,
-    signupVerifyToken: string
-  ) {
+  async sendMemberJoinVerification(emailAddress: string, signupVerifyToken: string) {
     const baseUrl = "http://localhost:4949"; // TODO: config
     const url = `${baseUrl}/auth/email-verify?signupVerifyToken=${signupVerifyToken}`;
 
